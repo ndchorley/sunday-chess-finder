858194e7a15d72f34935d8021e95465e3902dc54
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-05-18 06:51:36 +0100

    Add an import for URL in Event.java

--- src/main/java/com/xyphias/sundaychessfinder/Event.java
+++ src/main/java/com/xyphias/sundaychessfinder/Event.java
@@ -1,5 +1,6 @@
 package com.xyphias.sundaychessfinder;
 
+import java.net.URL;
 import java.time.LocalDate;
 
-public record Event(String name, LocalDate date, java.net.URL url) {}
+public record Event(String name, LocalDate date, URL url) {}
